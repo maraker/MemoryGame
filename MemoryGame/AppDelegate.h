cc071c88f0628e82a6babf0e939c74a88eab02cc
Markku Åkerfelt <Markku@Markkus-MacBook-Pro.local>
2015-02-09 22:43:21 +0200

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Markku Åkerfelt on 09/02/15.
+//  Copyright (c) 2015 Markku Åkerfelt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryGame
+//
+//  Created by Markku Åkerfelt on 09/02/15.
+//  Copyright (c) 2015 Markku Åkerfelt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
